27878681ab92b867c9d8cc52c1e37f0ce95af0c8
Gabriel Winkler <999gabriel.winkler@gmail.com>
2025-09-09 20:34:31 +0200

    very basic stuff here

---
+++ FirstConsoleApp/EorAchecker/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace EvenOrOddChecker;
+
+public class Checker
+{
+    private static int number;
+    private static bool isEven;
+    private static bool isOdd;
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter a number to check if it's even or odd: ");
+        while (true)
+        {
+            check();
+        }
+    }
+
+    public static void check()
+    {
+        number = Convert.ToInt32(Console.ReadLine());
+        
+        if (number % 2 == 0)
+        {
+            isEven = true;
+            Console.WriteLine("🥳 The number is even!");
+        }
+        else
+        {
+            isEven = false;
+            Console.WriteLine("🧐 The number is odd!");
+        }
+    }
+}
